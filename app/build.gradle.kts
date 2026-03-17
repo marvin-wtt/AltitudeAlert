879c6f7328b6511e02eae52671f5de9468da5e52
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-03-17 02:52:11 +0100

    feat: Support dark mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.google.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
